0b7ac70ca35118a8e066b28414a70ea5ae9eed45
Mar-Issah <masy370@gmail.com>
2023-12-01 23:47:05 -0600

    Feat: implement similarity search funtion

--- Youtube_assistant/langchain_helper.py
+++ Youtube_assistant/langchain_helper.py
@@ -28,6 +28,37 @@ def create_vector_db(video_url : str) -> FAISS:
   db = FAISS.from_documents(docs, OpenAIEmbeddings())
   return db
 
+# Similarity search
+def get_response_from_query(db, query, k=4):
+  # text-davinci max token is 4097
+
+  docs = db.similarity_search(query, k=k)
+  docs_content = ''.join([doc.page_content for doc in docs])
+
+  llm = OpenAI(model= "text-davinci-003")
+
+  prompt= PromptTemplate(
+    input_variables=["user_query", "docs"],
+    template="""
+    You are a helpful assistant that can answer questions about youtube videos based on the video's transcript.
+
+    Answer the following question: {user_query}
+    By searching the following video transcript: {docs}
+
+    Only use the factual information from the transcript to answer the question.
+
+    If you feel like you don't have enough information to answer the question, say "I don't know, Please ask another question".
+
+    Your answers should be very detailed.
+    """,
+  )
+
+# create and run our chain
+  chain = LLMChain(llm=llm, prompt=prompt)
+  response = chain.run(user_query=query, docs=docs_content)
+  response = response.replace("\n", "")
+  return response, docs
+
 
 if __name__ == "__main__":
   print(create_vector_db(video_url))
--- Youtube_assistant/main.py
+++ Youtube_assistant/main.py
@@ -1,27 +1,5 @@
 import streamlit as st
-from langchain_helper import generate_name
+import langchain_helper as lch
+import textwrap
 
-st.title("Pets Name Generator")
-
-
-# Sidebar UI
-animal_type = st.sidebar.selectbox("What is your pet?",("Dog", "Cat", "Rabbit", "Parrot"))
-
-if animal_type == "Dog":
-  pet_color = st.sidebar.text_area(label = "What color is your Dog?", max_chars=20)
-
-if animal_type == "Cat":
-  pet_color = st.sidebar.text_area(label = "What color is your Cat?",max_chars=20)
-
-if animal_type == "Rabbit":
-  pet_color = st.sidebar.text_area(label = "What color is your Rabbit?",max_chars=20)
-
-if animal_type == "Parrot":
-  pet_color = st.sidebar.text_area(label = "What color is your Parrot?",max_chars=20)
-
-
-
-# Use the user selection of type and color
-if pet_color:
-  response = generate_name(animal_type, pet_color)
-  st.text(response['pet_names'])
+st.title("YouTube Assistant")
