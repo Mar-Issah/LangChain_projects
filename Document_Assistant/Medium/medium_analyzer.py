8fa15651442642b2d760c250e855c8a22f44e51a
Mar-Issah <masy370@gmail.com>
2023-12-08 18:04:31 -0600

    Feat: implement text analyzer app

--- Document_Assistant/Medium/medium_analyzer.py
+++ Document_Assistant/Medium/medium_analyzer.py
@@ -7,11 +7,33 @@ from langchain.vectorstores import Pinecone
 # from langchain import VectorDBQA, OpenAI
 from langchain.chains import VectorDBQA
 from langchain.llms import OpenAI
-# RetrivalAQ/VectDBQA: This chain is designed for question-answering tasks where the answer is retrieved from a given context. It's useful when you have a specific document or set of documents that you want to extract information from.
-# import pinecone	# pip install pinecone-client
+import pinecone	# pip install pinecone-client
 # we are using pinecone vector database https://www.pinecone.io/
 
+pinecone.init(
+    api_key="280c8c37-efa2-4b2a-a372-a08fe61f698a",
+    environment="gcp-starter",
+)
 
 if __name__ == "__main__":
     print("Hello VectorStore!")
-    loader = TextLoader("C:/MARSIYA/PROGRAMMING/PORTFORLIO/ALL NOTES/AI/LANGCHAIN/LangChain_projects/Document_Assistant/Medium/mediumblog.txt",encoding="utf-8"
+
+    # First: load the document
+    loader = TextLoader("C:/MARSIYA/PROGRAMMING/PORTFORLIO/ALL NOTES/AI/LANGCHAIN/LangChain_projects/Document_Assistant/Medium/mediumblog.txt",encoding="utf-8")
+    document = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    docsearch = Pinecone.from_documents(
+        texts, embeddings, index_name="medium-blogs-embeddings-index"
+    )
+
+    qa = VectorDBQA.from_chain_type(
+        llm=OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY")), chain_type="stuff", vectorstore=docsearch, return_source_documents=True
+    )
+    query = "What is a vector DB? Give me a 15 word answer for a begginner"
+    result = qa({"query": query})
+    print(result)
+
